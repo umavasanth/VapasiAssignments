bb9a3cd48b7cceefb31dd1fe31f143e1e7647c0d
Umamaheswari <umavasanth27@gmail.com>
2021-11-13 20:05:36 +0530

    simpleCalc.java

--- simpleCalc.java
+++ simpleCalc.java
@@ -1,2 +1,40 @@
-package PACKAGE_NAME;public class simpleCalc {
+import java.util.Scanner;
+
+public class simpleCalc {
+    public static void main(String[] args) {
+        char ope;
+        double num1,num2,result;
+
+        Scanner inp=new Scanner(System.in);
+        System.out.println("Enter number1:  ");
+        System.out.println("Enter number2:  ");
+        num1=inp.nextDouble();
+        num2=inp.nextDouble();
+        System.out.println("Enter any operator(+, -, *, / )");
+
+        ope=inp.next().charAt(0);
+
+            switch (ope) {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    result = num1 / num2;
+                    break;
+                default:
+                    System.out.println("You have entered wrong operator");
+                    return;
+            }
+
+           inp.close();
+
+        System.out.println(num1+" " +ope+"" +num2+"="+result);
+
+    }
 }
